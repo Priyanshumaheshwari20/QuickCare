3e52fae91ced989b7cca3c5af11f10b586c26190
Aditya Daga <aadityadaga01@gmail.com>
2026-08-13 23:38:33 +0530

    Fix MongoDB catch error

--- client/Backend/config/db.js
+++ client/Backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async() => {
     try {
         await mongoose.connect("mongodb://127.0.0.1:27017/QuickCare");
         console.log("MongoDB Connected")
-    } catch (erorr) {
+    } catch (error) {
         console.error("MongoDB Connection Error:", error);
 
     }
